b57b3159f7fc4427649e3d638a0f2d1753b6142d
Romain Reuillon <romain.reuillon@iscpif.fr>
2011-07-15 14:55:06 +0200

    Encapsulate counting in class.

---
+++ src/main/scala/fr/iscpif/nlp/occurence/BloomFilter.scala
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr.iscpif.nlp.occurence
+
+object BloomFilter {
+ /* 
+  def apply(words: Iterable[String], hash: String => Int) = {
+    words.
+    val bits = new BitSet()
+  }
+  */
+}
+
+class BloomFilter {
+
+}
---
+++ src/main/scala/fr/iscpif/nlp/occurence/OccurenceCounter.scala
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr.iscpif.nlp.occurence
+
+class OccurenceCounter(dictionnary: Iterable[String], remove: Set[Char] = Set(',', '(', ')', ';', '.')) {
+    
+  def apply(text: String) = {
+    val lowerText = text.toLowerCase.filterNot(remove)
+    val index = Index(lowerText)
+
+    val occ = dictionnary.par.flatMap {
+      case w =>
+        index(w.split(' ').head).par.flatMap {
+          index => if(lowerText.slice(index, index + w.size) == w && endOfWord(lowerText, index + w.size)) List(w) else List()
+        }
+    }.groupBy{w => w}.map{case(w, l) => w -> l.size}.seq
+    
+      dictionnary.zipWithIndex.par.map{
+        case(l, i) => i -> splitExpressions(l).map{w => occ.getOrElse(w, 0)}.reduce(_+_)
+      }
+  }
+  
+  private def endOfWord(text: String, pos: Int) = (pos == text.size - 1) || (text(pos) == ' ' )
+  
+  private def splitExpressions(line: String) = line split("OR") map {_.trim.toLowerCase.filterNot(remove)}
+
+}
